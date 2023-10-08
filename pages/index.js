b0be02387c9cf021b5abc84851575194bc7afb40
Gerald <g.wagner@blitzen.de>
2023-10-08 10:23:16 +0200

    style: Minor Section, File and Asset Changes
    - Add Section Component
    - Remove Default Next Images
    - Remvoe Index Content

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
+import Layout from "../components/layout";
 
 export default function Home() {
-    return (
-        <Layout home>
-            <Head>
-                <title>{siteTitle}</title>
-            </Head>
-            <section className={utilStyles.headingMd}>
-                <h1 className={utilStyles.heading2Xl}>
-                    Welcome to the Coding Challenge <br />
-                    of <br />
-                    S-Com
-                </h1>
-                <Image
-                    src="/images/sparkasse-logo.svg" // Route of the image file
-                    height={144} // Desired size with correct aspect ratio
-                    width={144} // Desired size with correct aspect ratio
-                    alt="Sparkassen Logo"
-                />
-            </section>
-        </Layout>
-    );
+    return <Layout home></Layout>;
 }
