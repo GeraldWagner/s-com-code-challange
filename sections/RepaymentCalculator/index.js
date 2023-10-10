7feaeefcd4c12155abcfc8ba6dc67e0295128cc6
Gerald <g.wagner@blitzen.de>
2023-10-10 23:35:03 +0200

    docs: Add RepaymentCalculator index description

--- sections/RepaymentCalculator/index.js
+++ sections/RepaymentCalculator/index.js
@@ -1,8 +1,9 @@
+/* Parent */
 export { default as RepaymentCalculator } from "./RepaymentCalculator";
-
+/* components */
 export { default as CalculatorTable } from "./components/RepaymentTable";
 export { default as LoanInputForm } from "./components/LoanInputForm";
 export { default as ResultsDisplay } from "./components/ResultsDisplay";
-
+/* services */
 export { default as calculateRepaymentPlan } from "./services/calculateRepaymentPlan";
 export { formatCurrency } from "./services/formatter";
